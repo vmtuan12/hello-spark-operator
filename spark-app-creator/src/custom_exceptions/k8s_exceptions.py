d3c77880ee9c3d8023aef09ff79628cfb0dcb158
Tuan Vu Minh <minhtuan3154@gmail.com>
2025-11-16 20:07:45 +0700

    [feat] Create custom exceptions

---
+++ spark-app-creator/src/custom_exceptions/k8s_exceptions.py
@@ -0,0 +1,13 @@
+class PodFailedException(Exception):
+    pass
+
+
+class PermissionDeniedException(Exception):
+    pass
+
+
+class ResourceObjectNotFoundException(Exception):
+    def __init__(self, resource_type: str, message: str | None = None) -> None:
+        self.resource_type = resource_type
+        super().__init__(message)
+
